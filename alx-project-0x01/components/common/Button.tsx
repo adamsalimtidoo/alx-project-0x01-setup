017b342779c2b2f6aa9bcfc7145511555c9f0b6b
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-07-20 11:13:20 +0000

    updated

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type ButtonProps = {
+  title: string;
+  styles: string;
+};
+
+const Button = ({ title, styles }: ButtonProps) => {
+  return (
+    <button
+      className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition rounded-lg${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
